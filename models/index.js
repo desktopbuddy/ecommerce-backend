6be439f82747357e247424f2ee1b140af0d35d0f
desktopbuddy <hanchondo21@gmail.com>
2023-09-14 22:20:48 -0700

    Get all products minus associated tags

--- models/index.js
+++ models/index.js
@@ -17,11 +17,16 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'product_tag'
+  through: ProductTag,
+  foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'product_tag'
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
